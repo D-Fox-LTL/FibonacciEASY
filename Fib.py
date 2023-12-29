c1d20b9e00d3ecde6bdc0a537ad34bb55fbf12da
D-Fox-LTL <99818044+D-Fox-LTL@users.noreply.github.com>
2023-12-29 21:04:38 +0100

    Update Fib.py
    lol, made it even simplier

--- Fib.py
+++ Fib.py
@@ -1,18 +1,11 @@
 import sys
 import time
-
-
 a: int = 0
 b: int = 1
-c: int = 1
-
 while(True):
     print(f"{a}, ")
-    a = b+c
+    a = a+b
     time.sleep(1)
     print(f"{b}, ")
-    b = a+c
-    time.sleep(1)
-    print(f"{c}, ")
-    c = a+b
+    b = b+a
     time.sleep(1)
